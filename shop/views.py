b7f944e3be0c9520a891f69ebeb4100b524c98a8
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-19 01:16:22 +0900

    17 장고 Logging과 SQL Logging 처리

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,9 @@
+import logging
 from django.http import HttpResponse
 from django.shortcuts import render
-
 from shop.models import Item
 
+logger = logging.getLogger(__name__)
 
 def archives_year(request, year):
     return HttpResponse(f'{year}년도에 대한 내용')
@@ -13,6 +14,8 @@ def item_list(request):
     q = request.GET.get('q', '')
     if q:
         qs = qs.filter(title__icontains=q)
+
+    logger.debug(f'query : {q}')
 
     return render(request, 'shop/item_list.html', {
         'item_list': qs,
