1ab5bb5981fc960543e0474fe99d0b2bb9d10208
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-19 16:08:07 +0900

    22 템플릿 상속을 통한 중복제거

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 import logging
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from shop.models import Item
 
 logger = logging.getLogger(__name__)
@@ -20,4 +20,10 @@ def item_list(request):
     return render(request, 'shop/item_list.html', {
         'item_list': qs,
         'q': q,
+    })
+
+def item_detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'shop/item_detail.html', {
+        'item': item
     })
