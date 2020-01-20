7190a19076b33b18fdef7a6fb0a44f3ab0623799
KwonKusang <rnjsrntkd95@gmail.com>
2020-01-20 17:28:01 +0900

    29 장고 Form을 쓰지 않고, 글 생성/수정 구현하기

--- shop/models.py
+++ shop/models.py
@@ -15,6 +15,9 @@ class Item(models.Model):
     def __str__(self):
         return f'<{self.pk}> {self.title}'
 
+    # Models의 detail 파트에선 이 함수가 거의 필수임.
+    # redirect() 또는 resolve_url()이 먼저 get_absolute_url()의 존재여부를 먼저 체크하고 자동으로 호출
+    # 호출되면 리턴값으로 URL을 넘어가게 함
     def get_absolute_url(self):
         # return reverse('shop: item_detail', args=[self.pk])
         return reverse('shop:item_detail', kwargs={'pk': self.pk})
